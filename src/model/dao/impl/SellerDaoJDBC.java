9edda55021dba1212bc6bd109ad9d337213d14e7
theusknupp <matheus.knupp.ds@outlook.com>
2024-04-21 16:38:12 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -86,6 +89,48 @@ public class SellerDaoJDBC implements SellerDao{
 	@Override
 	public List<Seller> findAll() {
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null; //Consulta a ser feita
+		ResultSet rs = null; //Resultado da consulta
+		try {
+			st = conn.prepareStatement( //Consulta
+					"SELECT seller.*, department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+				    + "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			
+			st.setInt(1, department.getId()); //Valor que vai para "?"
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>(); //Map vazio, para guardar departamento instanciado
+			
+			while (rs.next()) { //testando se veio algum resultado -- whiller pois, pode vir mais de um resultado
+				
+				Department dep = map.get(rs.getInt("DepartmentId")); //Buscando no map se existe algum departamento com devido id
+				
+				if (dep == null) { //So instancia o departamento se for nullo, para não ter dois departamentos iguais
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep); //Salvando departamento no map
+				}
+				Seller obj = instantiateSeller(rs, dep); //Chamando função de instanciação de vendedor
+				list.add(obj);
+				
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());			
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}		
+		
 	}
 
 }
