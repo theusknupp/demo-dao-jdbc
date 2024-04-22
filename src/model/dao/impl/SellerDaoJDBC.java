894d457e15c38cea7a6fe9fa4a22c4b115fe8118
theusknupp <matheus.knupp.ds@outlook.com>
2024-04-21 21:13:13 -0300

    update implementation and fix errors

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -34,7 +34,7 @@ public class SellerDaoJDBC implements SellerDao{
 					+ "(?, ?, ?, ?, ?)", 
 					Statement.RETURN_GENERATED_KEYS);
 			
-			st.setString(1, obj.getEmail());
+			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());	
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
@@ -59,14 +59,31 @@ public class SellerDaoJDBC implements SellerDao{
 		}
 		finally {
 			DB.closeStatement(st);
-		}
-	
-		
+		}		
 	}
 
 	@Override
 	public void update(Seller obj) {
-	
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());	
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
