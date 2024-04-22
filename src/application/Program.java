a87f9c8b1a7722bedf159038ad00a6f1cabe9269
theusknupp <matheus.knupp.ds@outlook.com>
2024-04-21 21:45:41 -0300

    Implements DepartmentDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,10 +34,10 @@ public class Program {
 			System.out.println(obj);
 		}
 
-		/*System.out.println("\nTest 4, Seller insert: ");
+		System.out.println("\nTest 4, Seller insert: ");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());*/
+		System.out.println("Inserted! New id = " + newSeller.getId());
 		
 		System.out.println("\nTest 5, Seller update: ");
 		seller = sellerDao.findById(1);
